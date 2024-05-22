91f7ee4c45ace70218765ca78bfddc8d3c215815
Lerol49 <toleju04@gmail.com>
2024-05-22 08:43:15 +0200

    basic database integration with problems

--- app.py
+++ app.py
@@ -11,19 +11,20 @@ from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired, Length
 from wtforms import StringField, PasswordField, BooleanField
 from flask_bootstrap import Bootstrap
+from sqlalchemy.orm import attributes
 
 
 private = True
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=5, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=10)])
+    username = StringField('username', validators=[InputRequired(), Length(min=3, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=3, max=15)])
     remember = BooleanField('Remember me')
 
 
 class RegisterationForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=15)])
+    username = StringField('username', validators=[InputRequired(), Length(min=3, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=3, max=15)])
 
 
 
@@ -60,6 +61,11 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     password = db.Column(db.String(80))
+
+
+class Problems(db.Model):
+    username = db.Column(db.String(15), unique=True, primary_key=True)
+    pizza_distribution_problem = db.Column(db.JSON, default={"passed": False, "tries": 0})
 
 
 @login_manager.user_loader
@@ -102,18 +108,54 @@ def signup():
 
     return render_template("signup.html", form=form)
 
-def handle_task_submission(control_file) -> bool:
-    if request.method == "POST":
+def handle_task_submission(control_file, problem_name):
+    # checks if user has already submitted someting
+    user = Problems.query.filter_by(username=current_user.username).first()
+    if user is None:
+        new_try = Problems(username=current_user.username)
+        db.session.add(new_try)
+        db.session.commit()
+        return ""
+
+    elif request.method == "POST":
         submit_file = request.files["file"]
         if submit_file:
             submit_filename = secure_filename(submit_file.filename)
             submit_filepath = os.path.join(app.config["UPLOAD_DIRECTORY"], submit_filename)
             submit_file.save(submit_filepath)
             correct = compare_output_file(submit_filepath, control_file)
+            user = Problems.query.filter_by(username=current_user.username).first()
+            current_data = getattr(user, problem_name)
             if correct:
-                return current_user.username
+                current_data["passed"] = 1
+                current_data["tries"] += 1
+                setattr(user, problem_name, current_data)
+                attributes.flag_modified(user, problem_name)
+                db.session.commit()
+                return "richtig!"
             else:
+                current_data = getattr(user, problem_name)
+                current_data["passed"] = 0
+                current_data["tries"] += 1
+                setattr(user, problem_name, current_data)
+                attributes.flag_modified(user, problem_name)
+                db.session.commit()
                 return "falsch!"
+
+    elif getattr(user, problem_name).get("tries") == 0:
+        return ""
+
+    else:
+        problem = Problems.query.filter_by(username=current_user.username).first()
+        if getattr(problem, problem_name).get("passed"):
+            return "richtig"
+        else:
+            return "falsch"
+
+
+
+
+
     return ""
 
 
@@ -134,16 +176,23 @@ def login_info():
         return "<div><a href='login'>login</a>&#160&#160&#160 <a href='/signup'>registrieren</a></div>"
 
 @app.route("/test_contest")
+@login_required
 def test_contest():
     return render_template("/test_contest/test_contest_index.html", login_info=login_info())
 
 @app.route("/test_contest/pizza_distribution_problem", methods=["POST", "GET"])
+@login_required
 def pizza_distribution_problem():
-    result = handle_task_submission("templates/test_contest/cf_pizza_distribution_problem.txt")
+    result = handle_task_submission("templates/test_contest/cf_pizza_distribution_problem.txt", "pizza_distribution_problem")
     return render_template("/test_contest/pizza_distribution_problem.html", result=result)
 
+def reset_test_values():
+    pass
 
 
 if __name__ == "__main__":
+    # with app.app_context():
+    #    db.create_all()
     app.run(host=ip, port=8000, debug=True)
+
 
